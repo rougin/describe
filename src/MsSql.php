52b656bb509942e019022695634b56767f5f9a42
Rougin Royce Gutib <rougingutib@gmail.com>
2015-04-23 00:03:36 +0800

    Added MsSql.php

---
+++ src/MsSql.php
@@ -0,0 +1,51 @@
+<?php namespace Describe;
+
+use Describe\Describe;
+use Describe\DescribeInterface;
+
+/**
+ * MsSql Class
+ *
+ * @package  Describe
+ * @category MSSQL
+ * @author   Rougin Royce Gutib <rougingutib@gmail.com>
+ */
+class MsSql implements DescribeInterface {
+
+	private $_databaseHandle = NULL;
+
+	/**
+	 * Inject the database handle
+	 * 
+	 * @param \PDO $databaseHandle
+	 */
+	public function __construct($databaseHandle)
+	{
+		$this->_databaseHandle = $databaseHandle;
+	}
+
+	/**
+	 * Return the result
+	 * 
+	 * @return array
+	 */
+	public function getInformationFromTable($table)
+	{
+		$columns = array();
+
+		return $columns;
+	}
+
+	/**
+	 * Show the list of tables
+	 * 
+	 * @return array
+	 */
+	public function showTables()
+	{
+		$tables = array();
+
+		return $tables;
+	}
+
+}
