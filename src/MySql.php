993dce4b64dae7a4df82f66c720ce6103f71276a
Rougin Royce Gutib <rougingutib@gmail.com>
2015-09-03 23:41:57 +0800

    Updated MySql.php

--- src/MySql.php
+++ src/MySql.php
@@ -1,4 +1,6 @@
-<?php namespace Rougin\Describe;
+<?php
+
+namespace Rougin\Describe;
 
 use Rougin\Describe\DescribeInterface;
 use Rougin\Describe\Column;
@@ -10,111 +12,111 @@ use Rougin\Describe\Column;
  * @category MySQL
  * @author   Rougin Royce Gutib <rougingutib@gmail.com>
  */
-class MySql implements DescribeInterface {
+class MySql implements DescribeInterface
+{
+    protected $database;
+    protected $handle;
 
-	private $database;
-	private $handle;
+    /**
+     * Inject the database handle
+     * 
+     * @param \PDO   $handle
+     * @param string $database
+     */
+    public function __construct($handle, $database)
+    {
+        $this->handle = $handle;
+        $this->database = $database;
+    }
 
-	/**
-	 * Inject the database handle
-	 * 
-	 * @param \PDO   $handle
-	 * @param string $database
-	 */
-	public function __construct($handle, $database)
-	{
-		$this->handle = $handle;
-		$this->database = $database;
-	}
+    /**
+     * Return the result
+     * 
+     * @return array
+     */
+    public function getInformationFromTable($table)
+    {
+        $columns = [];
+        $query = 'DESCRIBE ' . $table;
 
-	/**
-	 * Return the result
-	 * 
-	 * @return array
-	 */
-	public function getInformationFromTable($table)
-	{
-		$columns = array();
-		$query = 'DESCRIBE ' . $table;
+        $tableInformation = $this->handle->prepare($query);
+        $tableInformation->execute();
+        $tableInformation->setFetchMode(\PDO::FETCH_OBJ);
 
-		$tableInformation = $this->handle->prepare($query);
-		$tableInformation->execute();
-		$tableInformation->setFetchMode(\PDO::FETCH_OBJ);
+        if (strpos($table, '.')) {
+            $table = substr($table, strpos($table, '.') + 1);
+        }
 
-		if (strpos($table, '.')) {
-			$table = substr($table, strpos($table, '.') + 1);
-		}
+        while ($row = $tableInformation->fetch()) {
+            preg_match('/(.*?)\((.*?)\)/', $row->Type, $match);
+            $column = new Column();
 
-		while ($row = $tableInformation->fetch()) {
-			preg_match('/(.*?)\((.*?)\)/', $row->Type, $match);
-			$column = new Column();
+            if ($row->Extra == 'auto_increment') {
+                $column->setAutoIncrement(TRUE);
+            }
 
-			if ($row->Extra == 'auto_increment') {
-				$column->setAutoIncrement(TRUE);
-			}
+            if ($row->Null == 'YES') {
+                $column->setNull(TRUE);
+            }
 
-			if ($row->Null == 'YES') {
-				$column->setNull(TRUE);
-			}
+            if ($row->Key == 'PRI') {
+                $column->setPrimary(TRUE);
+            } else if ($row->Key == 'MUL') {
+                $column->setForeign(TRUE);
+            } else if ($row->Key == 'UNI') {
+                $column->setUnique(TRUE);
+            }
 
-			if ($row->Key == 'PRI') {
-				$column->setPrimary(TRUE);
-			} else if ($row->Key == 'MUL') {
-				$column->setForeign(TRUE);
-			} else if ($row->Key == 'UNI') {
-				$column->setUnique(TRUE);
-			}
+            $column->setDefaultValue($row->Default);
+            $column->setField($row->Field);
 
-			$column->setDefaultValue($row->Default);
-			$column->setField($row->Field);
+            if (isset($match[1])) {
+                $column->setDataType($match[1]);
+                $column->setLength($match[2]);
+            } else {
+                $column->setDataType($row->Type);
+            }
 
-			if (isset($match[1])) {
-				$column->setDataType($match[1]);
-				$column->setLength($match[2]);
-			} else {
-				$column->setDataType($row->Type);
-			}
+            $query = 'SELECT COLUMN_NAME as "column", REFERENCED_COLUMN_NAME as "referenced_column",
+            CONCAT(REFERENCED_TABLE_SCHEMA, ".", REFERENCED_TABLE_NAME) as "referenced_table"
+            FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA = "' . $this->database . '"
+            AND TABLE_NAME = "' . $table . '";';
 
-			$query = 'SELECT COLUMN_NAME as "column", REFERENCED_COLUMN_NAME as "referenced_column",
-			CONCAT(REFERENCED_TABLE_SCHEMA, ".", REFERENCED_TABLE_NAME) as "referenced_table"
-			FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE CONSTRAINT_SCHEMA = "' . $this->database . '"
-			AND TABLE_NAME = "' . $table . '";';
+            $foreignTableInformation = $this->handle->prepare($query);
+            $foreignTableInformation->execute();
+            $foreignTableInformation->setFetchMode(\PDO::FETCH_OBJ);
 
-			$foreignTableInformation = $this->handle->prepare($query);
-			$foreignTableInformation->execute();
-			$foreignTableInformation->setFetchMode(\PDO::FETCH_OBJ);
+            while ($foreignRow = $foreignTableInformation->fetch()) {
+                if ($foreignRow->column == $row->Field) {
+                    $column->setReferencedField($foreignRow->referenced_column);
+                    $column->setReferencedTable($foreignRow->referenced_table);
+                }
+            }
 
-			while ($foreignRow = $foreignTableInformation->fetch()) {
-				if ($foreignRow->column == $row->Field) {
-					$column->setReferencedField($foreignRow->referenced_column);
-					$column->setReferencedTable($foreignRow->referenced_table);
-				}
-			}
+            $columns[] = $column;
+        }
 
-			$columns[] = $column;
-		}
+        return $columns;
+    }
 
-		return $columns;
-	}
+    /**
+     * Show the list of tables
+     * 
+     * @return array
+     */
+    public function showTables()
+    {
+        $tables = [];
+        $query = 'SHOW TABLES';
 
-	/**
-	 * Show the list of tables
-	 * 
-	 * @return array
-	 */
-	public function showTables()
-	{
-		$tables = array();
-		$query = 'SHOW TABLES';
+        $showTables = $this->handle->prepare($query);
+        $showTables->execute();
 
-		$showTables = $this->handle->prepare($query);
-		$showTables->execute();
+        while ($row = $showTables->fetch()) {
+            $tables[] = $row[0];
+        }
 
-		while ($row = $showTables->fetch()) {
-			$tables[] = $row[0];
-		}
-
-		return $tables;
-	}
+        return $tables;
+    }
 
 }
