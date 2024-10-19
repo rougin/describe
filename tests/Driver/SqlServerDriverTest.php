f0158c9896fae015a4c7eb111c3b62c1940508b5
Rougin Gutib <rougingutib@gmail.com>
2024-10-19 21:51:22 +0800

    Add unit test for SqlServerDriver

---
+++ tests/Driver/SqlServerDriverTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Rougin\Describe\Driver;
+
+use Rougin\Describe\Describe;
+
+/**
+ * @package Describe
+ *
+ * @author Rougin Gutib <rougingutib@gmail.com>
+ */
+class SqlServerDriverTest extends AbstractTestCase
+{
+    /**
+     * @return void
+     */
+    public function doSetUp()
+    {
+        $dsn = 'sqlsrv:server=127.0.0.1;Database=dscb';
+
+        $pdo = new \PDO($dsn, 'sa', 'dbatools.I0');
+
+        $driver = new SqlServerDriver($pdo);
+
+        $this->describe = new Describe($driver);
+    }
+}
